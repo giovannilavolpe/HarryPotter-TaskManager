526a5267e607da3b4067d14a773c61d1bb204622
mayragm2 <mayracueto@gm2dev.com>
2026-09-04 11:16:16 -0300

    feat(App.tsx): implement authentication context and main app structure

---
+++ HarryPotter-TaskManager-MMG/src/App.tsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import { useUsernameStore } from "./context/ContextAuth";
+import Auth from "./pages/auth/Auth";
+
+export default function App (){
+    const isLoggedIn = useUsernameStore((state) => state.isLoggedIn);
+    return(
+        <>
+        <Header/>
+        <main>
+            {!isLoggedIn && <Auth/>}
+        </main>
+        </>
+    )
+}
--- HarryPotter-TaskManager-MMG/src/main.tsx
+++ HarryPotter-TaskManager-MMG/src/main.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Header from './components/Header.tsx'
+import App from './App'
+
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <>
+      <App/>
+    </>
 
-    <header>
-        <Header/>
-    </header>
 
   </StrictMode>,
 )
