a5a29615d7ca6efd4e9bfc8aedc6d86dac6be69e
Giovanni La Volpe <giovanni.lavolpe@gm2dev.com>
2026-09-04 11:03:22 -0300

    feat(Context.tsx): add context & get fetch

---
+++ HarryPotter-TaskManager-MMG/src/context.tsx
@@ -0,0 +1,33 @@
+import { create } from 'zustand'
+
+interface Character {
+  image: string
+  id: string
+  name: string
+}
+
+interface CharacterState {
+  characters: Character[]
+  isLoading: boolean
+  ObtainCharacters: () => void
+}
+
+export const useCharacterStore = create<CharacterState>((set) => ({
+  characters: [],
+  isLoading: false,
+  
+  ObtainCharacters: async () => {
+
+    set({ isLoading: true })
+
+    try {
+      const response = await fetch('https://hp-api.onrender.com/api/characters/house/gryffindor')
+      const data: Character[] = await response.json()
+      set({ characters: data, isLoading: false })
+
+    } catch (error) {
+      console.error('Error al cargar personajes:', error)
+      set({ isLoading: false })
+    }
+  }
+}))
