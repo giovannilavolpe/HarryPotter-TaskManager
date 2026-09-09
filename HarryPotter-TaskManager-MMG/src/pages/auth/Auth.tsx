46517ab077d5163d335d7240706d97c4cd9c406e
mayragm2 <mayracueto@gm2dev.com>
2026-09-09 10:22:17 -0300

    fix(Auth): fix input field type usage and placeholder

--- HarryPotter-TaskManager-MMG/src/pages/auth/Auth.tsx
+++ HarryPotter-TaskManager-MMG/src/pages/auth/Auth.tsx
@@ -35,8 +35,8 @@ function Auth() {
 
         }}>
         <input  className= {backgroundTheme}
-                type="text"
-                placeholder="username"
+                type="email"
+                placeholder="Email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 style={{
@@ -47,8 +47,8 @@ function Auth() {
             />
             <input
                 className= {backgroundTheme}
-                type="text"
-                placeholder="password"
+                type="password"
+                placeholder="Password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 style={{
